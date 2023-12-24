59db76c1e36b3442a970f7cfee614044a54f1c3c
Tharushi <ththarushi3@gmail.com>
2023-12-24 17:20:42 +0530

    Start a new project - ProTask

---
+++ ProTask/src/main/java/com/Tharushi/ProTask/ProTaskApplication.java
@@ -0,0 +1,13 @@
+package com.Tharushi.ProTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProTaskApplication.class, args);
+	}
+
+}
---
+++ ProTask/src/test/java/com/Tharushi/ProTask/ProTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Tharushi.ProTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
